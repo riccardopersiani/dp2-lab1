92f176eb6370c3680dadacd683a0b288d56ca1f3
riccardopersiani <r.persiani92@gmail.com>
2016-11-24 11:50:48 +0100

    Adding the class NodeReaderUncomplete to support the lack of a NodeReader for instaziate LinkReader

---
+++ src/it/polito/dp2/NFFG/sol1/NodeReaderUncomplete.java
@@ -0,0 +1,28 @@
+package it.polito.dp2.NFFG.sol1;
+
+import java.util.Set;
+
+import it.polito.dp2.NFFG.FunctionalType;
+import it.polito.dp2.NFFG.LinkReader;
+import it.polito.dp2.NFFG.NodeReader;
+
+public class NodeReaderUncomplete implements NodeReader{
+	private String nodeName;
+	
+	public NodeReaderUncomplete(String nameNode){
+		this.nodeName = nameNode;
+	}
+	
+	public String getName() {
+		return this.nodeName;
+	}
+
+	public FunctionalType getFuncType() {
+		return null;
+	}
+
+	@Override
+	public Set<LinkReader> getLinks() {
+		return null;
+	}	
+}
